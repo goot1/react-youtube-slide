f147b01f7aa870a1cbf6fcc80758c49c3316f53a
nick901106 <nick901106@gmail.com>
2019-05-08 11:57:44 +0900

    version up

--- index.js
+++ index.js
@@ -8,3 +8,12 @@
 const Gallery = require('./react-youtube-slide/Gallery');
 
 module.exports = Gallery.default || Gallery;
+module.exports = () => ({
+    presets: [
+      require("@babel/preset-env"),
+    ],
+    plugins: [
+      [require("@babel/plugin-proposal-class-properties"), { loose: true }],
+      require("@babel/plugin-proposal-object-rest-spread"),
+    ],
+  });
